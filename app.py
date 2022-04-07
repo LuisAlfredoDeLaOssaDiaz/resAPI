b406d46c25a81af4c883dd82dc314f90c127638e
LuisAlfredoDeLaOssaDiaz <89400315+LuisAlfredoDeLaOssaDiaz@users.noreply.github.com>
2022-04-06 19:10:40 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,126 @@
+import requests
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+model = []
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/new_pet', methods=['GET'])
+def person():
+    return render_template('new_pet.html')
+
+
+@app.route('/pet_detail', methods=['POST'])
+def pet_detail():
+    api_url = "https://petstore.swagger.io/v2/pet"
+    id = request.form['id']
+    name = request.form['name']
+    new_data = {
+                    "id": id,
+                    "category": {
+                        "id": 0,
+                        "name": "string"
+                    },
+                    "name": name,
+                    "photoUrls": [
+                        "string"
+                    ],
+                    "tags": [
+                        {
+                            "id": 0,
+                            "name": "string"
+                        }
+                    ],
+                    "status": "available"
+                }
+    response = requests.post(api_url, json=new_data)
+    return render_template('pet_detail.html', value=(id, name))
+
+
+@app.route('/pet', methods=['GET'])
+def pets(status: str = "pending"):
+    url2 = "https://petstore.swagger.io/v2/pet/findByStatus?status={0}".format(status)
+    response = requests.get(url2)
+    data_pets = [(i['id'], i['name'], i['status']) for i in response.json()]
+    return render_template('pets.html', value=data_pets)
+
+
+@app.route('/pet_update/<id_pet>', methods=['GET'])
+def pet_update(id_pet):
+    return render_template('pet_update.html', value=id_pet)
+
+
+@app.route('/pet_update_detail', methods=['POST'])
+def pet_update_detail():
+    api_url_update = "https://petstore.swagger.io/v2/pet"
+    id = request.form['id']
+    name = request.form['name']
+    status = request.form['status']
+    update_data = {
+                        "id": id,
+                        "category": {
+                            "id": 0,
+                            "name": "string"
+                        },
+                        "name": name,
+                        "photoUrls": [
+                            "string"
+                        ],
+                        "tags": [
+                            {
+                                "id": 0,
+                                "name": "string"
+                            }
+                        ],
+                        "status": status
+                    }
+    response = requests.put(api_url_update, json=update_data)
+    return render_template('pet_detail.html', value=(id, name, status))
+
+
+@app.route('/pet_delete/<id_pet>', methods=['GET'])
+def pet_delete(id_pet):
+    api_url = "https://petstore.swagger.io/v2/pet/{0}".format(id_pet)
+    response = requests.delete(api_url)
+    return render_template('pet_detail.html', value="Delete successfully")
+
+
+
+
+
+@app.route("/stores")
+def stores():
+    return render_template('stores.html')
+
+
+@app.route('/stores', methods=['GET'])
+def stores(status: str = "placed"):
+    url3 = "https://petstore.swagger.io/v2/store/order/9".format(status)
+    response = requests.get(url3)
+    data_stores = [(i['id'], i['petId'], i['quantity'], i['shipDate'], i['status'], i['complete']) for i in response.json()]
+    return render_template('stores.html', value=data_stores)
+
+
+
+
+
+'''
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+'''
+if __name__ == '__main__':
+    app.run()
+
